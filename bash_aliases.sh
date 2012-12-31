040e43ef316bce5404b9ef14aaa2258a1e6c8889
Ewa Waliczek <ewa.waliczek@gmail.com>
2012-12-31 16:58:10 +0100

    aliases

---
+++ bash_aliases.sh
@@ -0,0 +1,16 @@
+alias play='sudo wvdial play'
+alias tssh='tsocks ssh'
+alias tscp='tsocks scp'
+
+alias ..='cd ..'
+alias be ='bundle exec'
+
+alias mci='mvn clean install'
+alias mi='mvn install'
+alias mist='mvn install -DskipTests=true -DskipFTs=true -DskipITs=true'
+alias mcist='mvn clean install -DskipTests=true -DskipFTs=true -DskipITs=true'
+
+alias gs='git status'
+alias gp='git pull'
+alias gl='git log'
+alias gpom='git push origin master'
